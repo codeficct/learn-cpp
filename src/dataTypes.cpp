c11c1f15ae48b9129413f7b311d2a990ac080bba
Luis Gabriel Janco <janco7249@gmail.com>
2023-02-28 14:06:22 -0400

    feat: initial commit

---
+++ src/dataTypes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    // Integer
+    int apples = 5;
+    int parking = -1;
+    // Float 32 bits - Double 64 bits
+    float positiveTransaction = 9.99;
+    float negativeTransaction = -23.08;
+    double price = 4.32;
+    double centimetres = -0.67;
+    // Boolean
+    bool isGreen = true; // 1
+    bool isRed = false; // 0
+    // Character - string
+    char letter = 'R';
+    string name = "Luis";
+    return 0;
+}
