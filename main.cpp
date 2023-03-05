0473614762a59b1a7fefcc11ce9d2cb08fee99e8
Luis Gabriel Janco <janco7249@gmail.com>
2023-03-04 21:30:54 -0400

    refacor: move main.cpp on root application

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "header.h"
+using namespace std;
+
+int main()
+{
+  // dataType();
+  // operations();
+  conditionalStructure();
+  cout << "Thanks for use my app :)" << endl;
+
+  return 0;
+}
