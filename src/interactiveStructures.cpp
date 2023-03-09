07b134685ff530a9e3f9efd51011e2f589de4f7a
Luis Gabriel Janco <janco7249@gmail.com>
2023-03-08 21:33:06 -0400

    feat: add interactive structure of loops

---
+++ src/interactiveStructures.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void interactiveStructures()
+{
+  cout << "[interactiveStructures] Enter a number interations: ";
+  int count;
+  cin >> count;
+
+  // For loop
+  for (int i = 0; i < count; i++)
+  {
+    cout << "(FOR) iteration = " << i << endl;
+  }
+
+  // While loop
+  int i = 0;
+  while (i < count)
+  {
+    cout << "(WHILE) iteration = " << i << endl;
+    i++;
+  }
+
+  // Do-while loop
+  int k = 0;
+  do
+  {
+    cout << "(DO-WHILE) iteration = " << k << endl;
+    k++;
+  } while (k < count);
+}
